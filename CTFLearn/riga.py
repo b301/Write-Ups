1cb20fd5c3115e8ec227b92b0908e68c8f335c2c
b301 <98285034+b301@users.noreply.github.com>
2022-03-02 09:01:36 +0200

    Update riga.py

--- CTFLearn/riga.py
+++ CTFLearn/riga.py
@@ -2,13 +2,32 @@
 
 __author__ = 0xb301
 __date__ = "2.3.22"
-__description__ = "It seems so easy once you solve it _-_"
+__description__ = "It seems so easy once you solve it _-_ In the end most of the functions were kind of useless?"
 
 # Globals
 imv: hex = 0xffffffff           #32bit
 
 def main():
-    secret = [
+    pickles0 = [
+        '9F', 'AE', '9C', 'B6', 
+        'BD', 'B9', 'EF', 'EB', 
+        'E6', '9E', 'B9', 'EC', 
+        'B3', 'B9', 'E3', 'B9', 
+        'BB', 'A8', '89', 'E3', 
+        'BD', 'EF', 'BB', '96', 
+        'B9', 'ED', 'E3', '89', 
+        'B9', 'E4'
+    ]
+    pickles1 = [
+        '9E', 'AD', '93', 'B5', 
+        'BC', 'B8', 'EE', 'EA', 
+        'E5', '90', 'BA', 'B8', 
+        'EB', 'BA', 'ED', 'E3', 
+        'E8', 'BC', 'EE', 'BA', 
+        'ED', 'EB', 'B8', 'EE', 
+        'EC', 'FB'
+    ]
+    pickles2 = [
         '9D', 'AC', '92', 'EB', 
         'B3', 'BF', 'ED', 'E9', 
         'E4', '97', 'B9', '94', 
@@ -16,15 +35,54 @@ def main():
         '94', 'BF', 'E3', 'E1', 
         'B7', 'BF', 'FF', 'FA'
     ]
-    solution(secret) 
-    return
+    tea0(pickles0)
+    tea1(pickles1) 
+    tea2(pickles2)
 
-# Solution
-def solution(secret: str) -> str:
+# Solutions [only 1 & 2 are accessible I believe? since beerBeverage is either 1 or 2]
+def tea0(secret: str) -> str:
     flag: str = ''
     original: list = []
     for i in secret:
-        eax = hex((int('0x'+i, 16)) ^ 0xffffffcb)
+        eax = hex((int('0x' + i, 16)) ^ 0xffffffcb)
+        eax = int(hex(int(eax, 16) + imv + 1)[-2:], 16)
+        if eax + 0x4e < 0x7e:
+            eax += 0x4e + 0x11
+
+        elif hex(eax - 0x5f & (2 ** 32 - 1))[-2:] == 0x20:
+            eax -= 0x64 #0x70-0x11 
+
+        eax -= 0x11
+        original.append(eax)
+        flag += chr(eax)
+    print(flag)
+    print(original)
+    return flag        
+    
+def tea1(secret: str) -> str:
+    flag: str = ''
+    original: list = []
+    for i in secret:
+        eax = hex((int('0x' + i, 16)) ^ 0xffffffcb)
+        eax = int(hex(int(eax, 16) + imv + 1)[-2:], 16)
+        if eax + 0x4d < 0x7e:
+            eax += 0x4d + 0x12
+
+        elif hex(eax - 0x5f & (2 ** 32 - 1))[-2:] == 0x20:
+            eax -= 0x5f #0x71-0x12 
+
+        eax -= 0x12
+        original.append(eax)
+        flag += chr(eax)
+    print(flag)
+    print(original)
+    return flag        
+
+def tea2(secret: str) -> str:
+    flag: str = ''
+    original: list = []
+    for i in secret:
+        eax = hex((int('0x' + i, 16)) ^ 0xffffffcb)
         eax = int(hex(int(eax, 16) + imv + 1)[-2:], 16)
         if eax + 0x4c < 0x7e:
             eax += 0x4c + 0x13
